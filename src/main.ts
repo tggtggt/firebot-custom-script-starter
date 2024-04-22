106ebe562a550225458511d838b0bd4de4a98104
tggtggt <tggtggt@gmail.com>
2024-04-22 15:38:43 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
+const { mouse, Point, Button } = require('@nut-tree/nut-js');
 
 interface Params {
   message: string;
@@ -7,9 +8,9 @@ interface Params {
 const script: Firebot.CustomScript<Params> = {
   getScriptManifest: () => {
     return {
-      name: "Starter Custom Script",
+      name: "movemousee",
       description: "A starter custom script for build",
-      author: "SomeDev",
+      author: "buster",
       version: "1.0",
       firebotVersion: "5",
     };
@@ -29,5 +30,56 @@ const script: Firebot.CustomScript<Params> = {
     logger.info(runRequest.parameters.message);
   },
 };
+
+  async function main() {
+    //who actually writes notes anyway, self explanatory code this time around
+    async function moveCursorTo(x, y) {
+        const position = new Point(x, y);
+        await mouse.setPosition(position);
+    }
+
+    async function leftClick(x, y) {
+        const position = new Point(x, y);
+        await mouse.setPosition(position);
+        await mouse.click(Button.LEFT);
+    }
+
+    async function rightClick(x, y) {
+        const position = new Point(x, y);
+        await mouse.setPosition(position);
+        await mouse.click(Button.RIGHT);
+    }
+
+    async function waitTime(ms) {
+        await new Promise(resolve => setTimeout(resolve, ms));
+    }
+  
+    await rightClick(2300, 640);
+    await waitTime(100);
+    await leftClick(2350, 585);
+    await waitTime(100);
+    await leftClick(2800, 600);
+    await waitTime(100);
+    await leftClick(3180, 230);
+  }
+
+exports.getScriptManifest = () => ({
+    firebotVersion: "5"
+});
+
+function run(runRequest) {
+    // Return a Promise object
+    return new Promise((resolve, reject) => {
+    //your code here 
+    main();
+    });
+  }
+
+
+exports.main = main;
+
+exports.run = run;
+run();
+
 
 export default script;
